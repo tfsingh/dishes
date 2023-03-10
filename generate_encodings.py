361b24717b7909ef8dc96ba9d7632be6fd70209f
tfsingh <tejfsingh@gmail.com>
2023-03-09 19:00:22 -0800

    renamed file

---
+++ generate_encodings.py
@@ -0,0 +1,20 @@
+import os
+import face_recognition
+
+#create an image and an encoding for each file in the faces folder
+def create_encodings():
+    encodings = []
+    for file in os.listdir("faces"):
+        image = face_recognition.load_image_file("faces/" + file)
+        encoding = face_recognition.face_encodings(image)[0]
+        encodings.append(encoding)
+    return encodings
+
+#return an array of names of people in the faces folder
+def create_names():
+    names = []
+    for file in os.listdir("faces"):
+        name = file.split(".")[0]
+        names.append(name)
+    return names
+
